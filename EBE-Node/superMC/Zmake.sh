59ec75235d7d742163c6e338d8b088c1e2d1b7f6
Chun Shen <shen.201@asc.ohio-state.edu>
2013-03-19 11:26:10 -0400

    Change all cxx to cpp and add make file for superMC

--- EBE-Node/superMC/Zmake.sh
+++ EBE-Node/superMC/Zmake.sh
@@ -16,4 +16,4 @@ if [ "$FC" == "" ]; then
   FC=`which gfortran`; FFLAGS=" -O3 -cpp"
 fi
 
-$CC main.cxx Bases.cxx MCnucl.cxx GlueDensity.cxx MakeDensity.cxx KLNModel.cxx OverLap.cxx Largex.cxx Regge96.cxx rcBKfunc.cxx MathBasics.cpp ParameterReader.cpp arsenal.cpp EOS.cpp GaussianNucleonsCal.cpp NBD.cpp RandomVariable.cpp TableFunction.cpp Table.cpp -o superMC.e $CFLAGS
+$CC main.cpp Bases.cpp MCnucl.cpp GlueDensity.cpp MakeDensity.cpp KLNModel.cpp OverLap.cpp Largex.cpp Regge96.cpp rcBKfunc.cpp MathBasics.cpp ParameterReader.cpp arsenal.cpp EOS.cpp GaussianNucleonsCal.cpp NBD.cpp RandomVariable.cpp TableFunction.cpp Table.cpp -o superMC.e $CFLAGS
