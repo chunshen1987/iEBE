1cbda44c0cca3fae99af8f2fec00d5677e979002
Zhi Qiu <qiu.24@asc.ohio-state.edu>
2013-03-23 09:42:07 -0400

    The last test version as 03-22-2013.

--- EBE-Node/ResultCollector/DBR.py
+++ EBE-Node/ResultCollector/DBR.py
@@ -29,9 +29,7 @@ class SqliteDB(object):
             Register the file with "fileName" as the database file.
         """
         self._registeredDatabase = None # stores the filename for the database
-        self._dbCon = False # reference to the database connection
-        self._deletionSafety = True # extra caution will be used during deletion when set to True
-        self._counter = 0 # debugging
+        self._dbCon = None # reference to the database connection
         self.registerDatabase(fileName)
 
     def getRegisteredDatabase(self):
@@ -42,39 +40,32 @@ class SqliteDB(object):
 
     def registerDatabase(self, fileName):
         """
-            Register a database filename. Note that this action will
-            delteion-safety lock the newly registered database, even if the
-            newly registered database is the same as the old one.
+            Register and open a database file with "filename".
         """
-        self._registeredDatabase = fileName # used to store the name of the database
-        self._deletionSafety = True
+        self.closeConnection()
+        self._registeredDatabase = fileName # used to store the name of the registered database
+        self._openConnection()
 
     def _openConnection(self):
         """
-            Open a connection to a database file. Returns True upon success.
+            Open a connection to a database file.
         """
         # check if database is registerred
-        if not self._registeredDatabase: raise self.SqliteDBError("database not registerd")
-        # close opened connection to prevent port leakage
-        self._closeConnection()
-        # throw exceptions upon errors
-        self._dbCon = sqlite3.connect(self._registeredDatabase)
-        return True
+        if not self._registeredDatabase:
+            raise self.SqliteDBError("database not registerd")
+        # check if database already open
+        if not self._dbCon:
+            self._dbCon = sqlite3.connect(self._registeredDatabase)
 
-    def _closeConnection(self):
+    def closeConnection(self):
         """
             Close the current connection. Returns False if no connection was
             opened. All modification will be saved upon closure.
         """
         if self._dbCon:
-            self._dbCon.commit()
-            self._counter += 1
-            print(self._counter)
+            self._dbCon.commit() # write to disk only upon closure
             self._dbCon.close()
             self._dbCon = None
-            return True
-        else:
-            return False
 
     def _executeSQL(self, cmdString, parameterTuple=(), many=False):
         """
@@ -87,7 +78,7 @@ class SqliteDB(object):
         """
         # check if the connection is open
         if not self._dbCon:
-            self._openConnection()
+            self.openConnection()
         # execute,and throw exceptions if error occurs
         try:
             if not many:
@@ -104,18 +95,14 @@ class SqliteDB(object):
         """
             Return a list of table names from the registered database.
         """
-        self._openConnection()
         return [item[1] for item in self._executeSQL("select * from sqlite_master") if item[0]=="table"] # returned item: (type, name, tbl_name, rootpage, sql)
-        self._closeConnection()
 
     def getTableInfo(self, tableName):
         """
             Return a list of the form ('field', 'type') for all fields in the
             table "tableName".
         """
-        self._openConnection()
         return [ (item[1],item[2]) for item in self._executeSQL("pragma table_info(%s)" % tableName) ]
-        self._closeConnection()
 
     def doesTableExist(self, tableName):
         """
@@ -149,7 +136,7 @@ class SqliteDB(object):
         else:
             return False
 
-    def insertIntoTable(self, tableName, valueList, nameList=None, dataTypeStringList=None):
+    def insertIntoTable(self, tableName, valueList):
         """
             Insert values from "valueList" into the table with name "tableName".
             The inserted values have name list "nameList" and types specified in
@@ -160,33 +147,19 @@ class SqliteDB(object):
             nameList = ["id"]
             dataTypeStringList = ["int"]
 
-            The table will be created if it does not exist; otherwise the
-            existing table with the same name will be used, in which case the
-            arguments "nameList" and "dataTypeStringList" can be ignored.
+            The table has to be already created (not checked).
 
-            Return False if insertion failed.
         """
-        if not self.doesTableExist(tableName):
-            if nameList:
-                self.createTableIfNotExists(tableName, nameList, dataTypeStringList)
-            else:
-                raise self.SqliteDBError("table does not exist and cannot be created because name/type list not given")
-        if self.doesTableExist(tableName):
+        # make valueList doubly nested
+        if not ListRNew.isIterable(valueList):
+            valueList = [valueList]
+        if not ListRNew.isIterable(valueList[0]):
+            valueList = [[x] for x in valueList] # nest the level 1 list to level 2
 
-            # make valueList doubly nested
-            if not ListRNew.isIterable(valueList):
-                valueList = [valueList]
-            if not ListRNew.isIterable(valueList[0]):
-                valueList = [[x] for x in valueList] # nest the level 1 list to level 2
-
-            # perform SQL
-            dataLength = len(valueList[0]) # get number of elements in a row
-            self._openConnection() # open connection
-            returnValue = self._executeSQL("insert into %s values (%s)" % (tableName, ",".join("?"*dataLength)), valueList, many=True) # perform insertion
-            self._closeConnection() # close connection
-            return returnValue
-        else:
-            return False # somehow the table cannot be created or was deleted just before insertion
+        # perform SQL
+        dataLength = len(valueList[0]) # get number of elements in a row
+        returnValue = self._executeSQL("insert into %s values (%s)" % (tableName, ",".join("?"*dataLength)), valueList, many=True) # perform insertion
+        return returnValue
 
 
     def selectFromTable(self, tableName, columnNameList="*", whereClause=""):
@@ -201,46 +174,36 @@ class SqliteDB(object):
             columnNameList = [columnNameList]
         columnNameListSQLString = ",".join(columnNameList) # make is SQL-like
         # perform SQL
-        self._openConnection()
         sqlCommand = "select %s from %s" % (columnNameListSQLString, tableName)
         if whereClause:
             sqlCommand += " where " + whereClause
         returnValue = self._executeSQL(sqlCommand).fetchall()
-        self._closeConnection()
         return returnValue
 
-    def dropTable(self, tableName, overrideSafety=False):
+    def dropTable(self, tableName):
         """
-            Delete the table with name "tableName". When self._deletionSafety is
-            set to True, the argument "overrideSafty" needs to be set to True in
-            order for deletion to happen. Return True upon success; return False
-            if table does not exist or deletion is not permitted.
+            Delete the table with name "tableName". Return True upon success; return False
+            if table does not exist.
         """
-        # check safety lock
-        if self._deletionSafety and not overrideSafety:
-            raise self.SqliteDBError("deletion prevented by the internal lock")
         # check table existence
         if tableName not in self.getAllTableNames():
             return False
         # perform SQL
-        self._openConnection()
         self._executeSQL("drop table %s" % tableName)
-        self._closeConnection()
         return True
 
-    def deleteDatabase(self, overrideSafety=False):
+    def deleteDatabase(self, confirmation=True):
         """
-            Delete the file associated to the currently registered database.
-            When self._deletionSafety is set to True, the argument
-            "overrideSafety" needs to be set to True in order for deletion to
-            happen.
+            Delete the file associated to the currently registered database. The
+            argument confirmation needs to be set to True in order for deletion
+            to actually happen.
 
             Return False if the database file is not found or database is not
             registered; return True if the deletion is completed normally.
         """
         # check safety lock
-        if self._deletionSafety and not overrideSafety:
-            raise self.SqliteDBError("deletion prevented by the internal lock")
+        if not confirmation:
+            raise self.SqliteDBError("deletion not confirmed")
         # check if the database is registered
         db = self.getRegisteredDatabase()
         if not db: return False
