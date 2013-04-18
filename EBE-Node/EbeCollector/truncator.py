694b6afe72fe1acf47f8407bd9701ed7f1605c1b
Zhi Qiu <qiu.24@asc.ohio-state.edu>
2013-04-18 13:52:20 -0400

    2013.04.18.1: utilities/demo.db updated.

---
+++ EBE-Node/EbeCollector/truncator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from DBR import SqliteDB
+
+fromDB = SqliteDB("/home/qiu/Downloads/Pb_0_5_Glb_More_2000.db")
+toDB = SqliteDB("/home/qiu/Downloads/Pb_0_5_Glb_More_2000_truncated.db")
+
+for aTable in fromDB.getAllTableNames():
+    print(aTable)
+    # first copy table structure
+    firstCreation = toDB.createTableIfNotExists(aTable, fromDB.getTableInfo(aTable))
+    if "lookup" in aTable: # just copy
+        toDB.insertIntoTable(aTable, fromDB.selectFromTable(aTable))
+    else: # truncate
+        toDB.insertIntoTable(aTable, fromDB.selectFromTable(aTable, whereClause="event_id<=1000"))
+toDB.closeConnection() # commit
