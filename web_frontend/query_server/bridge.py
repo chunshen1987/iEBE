4ae4a0a984771210c911015949ed4dd5a05d5f82
Chih Chiu <Chih.Chiu.19@gmail.com>
2014-02-08 21:41:56 -0800

    1st version.

--- web_frontend/query_server/bridge.py
+++ web_frontend/query_server/bridge.py
@@ -1,24 +1,29 @@
+from os import path
 from EbeCollector import EbeDBReader
 
 
 # The following strings are used as query parameters.
-EXPR = "expr"
-DATABASE = "database"
+EXPR_PARAM = "expr"
+DATABASE_PARAM = "database"
 
-DATABASE_PATH = "../databases"
+DATABASE_RELATIVE_PATH = "databases"
 
 
 class QueryBridge(object):
     def __init__(self, database_name=""):
-        self.database_name = None
+        self.database_name = ""
         self.reader = None
 
         self.set_datebase(database_name)
 
     def set_database(self, database_name):
+        if not database_name.endwith(".db"):
+            database_name += ".db"
+
         if database_name != self.database_name:
             self.database_name = database_name
-            self.reader = EbeDBReader(database_name)
+            self.reader = EbeDBReader(path.join(DATABASE_RELATIVE_PATH,
+                                                database_name))
 
     def evaluate_expression(self, expr):
         return self.reader.evaluateExpressionOnly(expr)
--- web_frontend/query_server/views.py
+++ web_frontend/query_server/views.py
@@ -3,14 +3,24 @@ from django.http import HttpResponse
 
 import bridge
 
+query_bridge = bridge.QueryBridge()
+
+print
+print "Hello"
+print
+
 
 def query(request):
-    expression = request.GET.get("expr", "")
+    database_name = request.GET.get(bridge.DATABASE_PARAM, "")
+    expression = request.GET.get(bridge.EXPR_PARAM, "")
+
     if not expression:
-        return HttpResponse("Syntax: /query?expr=expression")
+        return HttpResponse("Syntax: /query?%s=database_name&%s=expression" %
+                            (bridge.DATABASE_PARAM, bridge.EXPR_PARAM))
     else:
-        print expression
-        return HttpResponse(expression)
+        print database_name, expression
+        query_bridge.set_database(database_name)
+        return HttpResponse(query_bridge.evaluate_expression(expression))
 
 
 def home(unused_request):
