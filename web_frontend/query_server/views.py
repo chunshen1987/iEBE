6740a2f00c84253c9d867613e5815c3f0fc9292d
Chih Chiu <Chih.Chiu.19@gmail.com>
2014-02-08 19:12:22 -0800

    Add query url.

--- web_frontend/query_server/views.py
+++ web_frontend/query_server/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def query(request):
+    return HttpResponse("Hello, world. You're at the poll index.")
