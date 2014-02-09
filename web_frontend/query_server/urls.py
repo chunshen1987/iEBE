ebaefcd6ed4297a20bc62f4af218a1f85d9e8ceb
Chih Chiu <Chih.Chiu.19@gmail.com>
2014-02-08 20:18:47 -0800

    Refined urls.

--- web_frontend/query_server/urls.py
+++ web_frontend/query_server/urls.py
@@ -5,8 +5,7 @@ from query_server import views
 
 urlpatterns = patterns(
     '',
-    url(r'$', views.home, name='query'),
-    url(r'home/$', views.home, name='query'),
-    url(r'query/$', views.query, name='query'),
-    url(r'query(?<params>.*)', views.query, name='query'),
+    url(r'^home/$', views.home, name='query'),
+    url(r'^query$', views.query, name='query'),
+    url(r'^$', views.home, name='query'),
 )
--- web_frontend/web_frontend/urls.py
+++ web_frontend/web_frontend/urls.py
@@ -4,10 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'web_fontend.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'', include('query_server.urls')),
