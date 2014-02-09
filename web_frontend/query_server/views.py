6d66030fb66837ecf53497bf97c4a87e237d420d
Chih Chiu <Chih.Chiu.19@gmail.com>
2014-02-08 21:10:08 -0800

    Add path to lib folder.

--- web_frontend/query_server/views.py
+++ web_frontend/query_server/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import EbeCollector
 
 def query(request):
     expression = request.GET.get("expr", "")
---
+++ web_frontend/web_frontend/__init__.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+lib_path = os.path.abspath("../utilities")
+if lib_path not in sys.path:
+    sys.path.append(lib_path)
+
