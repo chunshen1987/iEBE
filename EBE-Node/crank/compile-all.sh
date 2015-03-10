c5d18c8375d882596956eb683ea29b88136b7fed
Chun Shen <chunshen1987@gmail.com>
2015-03-10 16:26:35 -0400

    add free streaming package in the compile all

--- EBE-Node/crank/compile-all.sh
+++ EBE-Node/crank/compile-all.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 (cd ..
-for ii in superMC VISHNew photonEmission iSS iS osc2u urqmd
+for ii in superMC VISHNew photonEmission iSS iS osc2u urqmd fs
     do
     (cd $ii; make; make clean)
 done
