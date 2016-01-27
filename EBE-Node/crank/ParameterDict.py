ff4bdc0d0a543032e9a67473a233fa8ea2264be6
Chun Shen <chunshen1987@gmail.com>
2016-01-27 09:54:42 -0500

    change the default settings in the ParameterDict.py

--- EBE-Node/crank/ParameterDict.py
+++ EBE-Node/crank/ParameterDict.py
@@ -10,7 +10,7 @@ initial_condition_control = {
     'centrality': '30-40%',  # centrality bin
     'cut_type': 'total_entropy',
     # centrality cut variable: total_entropy or Npart
-    'initial_condition_type': 'pre-generated',
+    'initial_condition_type': 'superMC',
     # type of initial conditions: superMC or pre-generated
     'pre-generated_initial_file_path': 'initial_conditions', 
     # file path for the pre-generated initial condition files
